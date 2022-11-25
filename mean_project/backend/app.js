fb07af346686a77e18e9caf56bd822f4c5c94904
WHITELOTUS0 <84666504+WHITELOTUS0@users.noreply.github.com>
2022-11-25 17:15:19 +0300

    more functionalities

--- mean_project/backend/app.js
+++ mean_project/backend/app.js
@@ -1,24 +1,51 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('/api/posts',(req, res, next) => {
-  const posts=[
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
+  const posts = [
     {
-      id: 'fadf12421',
-      title:"First server-side post",
-      content: 'This is coming from the server'
+      id: "fadf12421l",
+      title: "First server-side post",
+      content: "This is coming from the server"
     },
     {
-      id: 'kddgdyet45464',
-      title:"Second server-side post",
-      content: 'This is coming from the server'
+      id: "ksajflaj132",
+      title: "Second server-side post",
+      content: "This is coming from the server!"
     }
   ];
   res.status(200).json({
-    message: "Posts fetched succesfully",
-    posts:posts
+    message: "Posts fetched successfully!",
+    posts: posts
   });
 });
 
 module.exports = app;
+
+//FWltwz7uU9WHK6LC
