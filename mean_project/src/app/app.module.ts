c06edbcd7c044357d4d179c8b92fec39a9e07924
WHITELOTUS0 <84666504+WHITELOTUS0@users.noreply.github.com>
2022-11-25 11:07:29 +0300

    connecting angular to backend

--- mean_project/src/app/app.module.ts
+++ mean_project/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -33,7 +34,8 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- mean_project/src/app/posts/post-list/post-list.component.ts
+++ mean_project/src/app/posts/post-list/post-list.component.ts
@@ -20,8 +20,8 @@ export class PostListComponent implements OnInit{
 
   constructor(public postsService: PostsService){}
 
-  ngOnInit(): void {
-      this.posts= this.postsService.getPosts();
+  ngOnInit(){
+      this.postsService.getPosts();
       this.postsSub = this.postsService.getPostUpdateListener()
       .subscribe((posts:Post[])=>{
         this.posts=posts;
