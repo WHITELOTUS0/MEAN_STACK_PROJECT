e4f82ab4b93f26a09dbd2b9419d3dae2af3575e7
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2022-12-13 15:38:51 +0300

    Error Handling

--- mean_project/backend/models/user.js
+++ mean_project/backend/models/user.js
@@ -1,14 +1,11 @@
-const mongoose = require('mongoose');
-const mongooseUniqueValidator = require('mongoose-unique-validator');
-const unique = require("mongoose-unique-validator");
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
-  email:{type:String, required:true, unique:true},
-  password:{type:String, required:true},
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
-userSchema.plugin(mongooseUniqueValidator);
+userSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model('User', userSchema);
-
-
+module.exports = mongoose.model("User", userSchema);
--- mean_project/src/app/posts/posts.service.ts
+++ mean_project/src/app/posts/posts.service.ts
@@ -9,32 +9,38 @@ import { Post } from "./post.model";
 @Injectable({ providedIn: "root" })
 export class PostsService {
   private posts: Post[] = [];
-  private postsUpdated = new Subject<{posts: Post[], postCount:number}>();
+  private postsUpdated = new Subject<{ posts: Post[]; postCount: number }>();
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  getPosts(postsPerPage:number, currentPage:number) {
-    const queryParams=`?pagesize=${postsPerPage}&page=${currentPage}`;
+  getPosts(postsPerPage: number, currentPage: number) {
+    const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
     this.http
-      .get<{ message: string; posts: any, maxPosts:number }>("http://localhost:3000/api/posts" + queryParams)
+      .get<{ message: string; posts: any; maxPosts: number }>(
+        "http://localhost:3000/api/posts" + queryParams
+      )
       .pipe(
         map(postData => {
-          return {posts: postData.posts.map(post => {
-            return {
-              title: post.title,
-              content: post.content,
-              id: post._id,
-              imagePath: post.imagePath
-            };
-          }), maxPosts: postData.maxPosts
-        };
+          return {
+            posts: postData.posts.map(post => {
+              return {
+                title: post.title,
+                content: post.content,
+                id: post._id,
+                imagePath: post.imagePath,
+                creator: post.creator
+              };
+            }),
+            maxPosts: postData.maxPosts
+          };
         })
       )
       .subscribe(transformedPostData => {
         this.posts = transformedPostData.posts;
         this.postsUpdated.next({
-           posts: [...this.posts],
-           postCount: transformedPostData.maxPosts});
+          posts: [...this.posts],
+          postCount: transformedPostData.maxPosts
+        });
       });
   }
 
@@ -43,9 +49,13 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return this.http.get<{ _id: string, title: string, content: string, imagePath: string }>(
-      "http://localhost:3000/api/posts/" + id
-    );
+    return this.http.get<{
+      _id: string;
+      title: string;
+      content: string;
+      imagePath: string;
+      creator: string;
+    }>("http://localhost:3000/api/posts/" + id);
   }
 
   addPost(title: string, content: string, image: File) {
@@ -59,7 +69,6 @@ export class PostsService {
         postData
       )
       .subscribe(responseData => {
-
         this.router.navigate(["/"]);
       });
   }
@@ -77,20 +86,18 @@ export class PostsService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       };
     }
     this.http
       .put("http://localhost:3000/api/posts/" + id, postData)
       .subscribe(response => {
-
         this.router.navigate(["/"]);
       });
   }
 
   deletePost(postId: string) {
-    return this.http
-      .delete("http://localhost:3000/api/posts/" + postId);
-
+    return this.http.delete("http://localhost:3000/api/posts/" + postId);
   }
 }
